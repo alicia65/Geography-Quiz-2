7437d37ed287eecdce4cdefb532393ad492f91c3
alicia65 <alicia65>
2020-04-04 22:28:41 -0500

    Add project files.

---
+++ Geography Quiz 2/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Geography_Quiz_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion1 = new System.Windows.Forms.Label();
+            this.lblQuestion2 = new System.Windows.Forms.Label();
+            this.lblQuestion3 = new System.Windows.Forms.Label();
+            this.txtAnswer1 = new System.Windows.Forms.TextBox();
+            this.txtAnswer2 = new System.Windows.Forms.TextBox();
+            this.txtAnswer3 = new System.Windows.Forms.TextBox();
+            this.btnCheckAnswer = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblQuestion1
+            // 
+            this.lblQuestion1.Location = new System.Drawing.Point(12, 36);
+            this.lblQuestion1.Name = "lblQuestion1";
+            this.lblQuestion1.Size = new System.Drawing.Size(267, 23);
+            this.lblQuestion1.TabIndex = 0;
+            this.lblQuestion1.Text = "What is the state capital of California? ";
+            this.lblQuestion1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblQuestion1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblQuestion2
+            // 
+            this.lblQuestion2.Location = new System.Drawing.Point(12, 100);
+            this.lblQuestion2.Name = "lblQuestion2";
+            this.lblQuestion2.Size = new System.Drawing.Size(267, 23);
+            this.lblQuestion2.TabIndex = 1;
+            this.lblQuestion2.Text = "What is the tallest mountain in the world? ";
+            this.lblQuestion2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblQuestion3
+            // 
+            this.lblQuestion3.Location = new System.Drawing.Point(12, 160);
+            this.lblQuestion3.Name = "lblQuestion3";
+            this.lblQuestion3.Size = new System.Drawing.Size(301, 23);
+            this.lblQuestion3.TabIndex = 2;
+            this.lblQuestion3.Text = "Which country has the furthest south extent?";
+            this.lblQuestion3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtAnswer1
+            // 
+            this.txtAnswer1.Location = new System.Drawing.Point(446, 45);
+            this.txtAnswer1.Name = "txtAnswer1";
+            this.txtAnswer1.Size = new System.Drawing.Size(179, 22);
+            this.txtAnswer1.TabIndex = 0;
+            // 
+            // txtAnswer2
+            // 
+            this.txtAnswer2.Location = new System.Drawing.Point(446, 99);
+            this.txtAnswer2.Name = "txtAnswer2";
+            this.txtAnswer2.Size = new System.Drawing.Size(179, 22);
+            this.txtAnswer2.TabIndex = 1;
+            // 
+            // txtAnswer3
+            // 
+            this.txtAnswer3.Location = new System.Drawing.Point(446, 160);
+            this.txtAnswer3.Name = "txtAnswer3";
+            this.txtAnswer3.Size = new System.Drawing.Size(179, 22);
+            this.txtAnswer3.TabIndex = 2;
+            // 
+            // btnCheckAnswer
+            // 
+            this.btnCheckAnswer.Location = new System.Drawing.Point(283, 229);
+            this.btnCheckAnswer.Name = "btnCheckAnswer";
+            this.btnCheckAnswer.Size = new System.Drawing.Size(132, 23);
+            this.btnCheckAnswer.TabIndex = 3;
+            this.btnCheckAnswer.Text = "Check";
+            this.btnCheckAnswer.UseVisualStyleBackColor = true;
+            this.btnCheckAnswer.Click += new System.EventHandler(this.btnCheckAnswer_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 326);
+            this.Controls.Add(this.btnCheckAnswer);
+            this.Controls.Add(this.txtAnswer3);
+            this.Controls.Add(this.txtAnswer2);
+            this.Controls.Add(this.txtAnswer1);
+            this.Controls.Add(this.lblQuestion3);
+            this.Controls.Add(this.lblQuestion2);
+            this.Controls.Add(this.lblQuestion1);
+            this.Name = "Form1";
+            this.Text = "Geography Quiz 2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion1;
+        private System.Windows.Forms.Label lblQuestion2;
+        private System.Windows.Forms.Label lblQuestion3;
+        private System.Windows.Forms.TextBox txtAnswer1;
+        private System.Windows.Forms.TextBox txtAnswer2;
+        private System.Windows.Forms.TextBox txtAnswer3;
+        private System.Windows.Forms.Button btnCheckAnswer;
+    }
+}
+
---
+++ Geography Quiz 2/Form1.cs
@@ -0,0 +1,84 @@
+﻿/* Geography Quiz 2 (Design TEAM, code individual)
+
+Start a new app.
+
+    This app will ask at least three geography questions, one question at a time.
+    The app will keep track of how many questions the user answers correctly. 
+    Your app will need to track how long the user took to complete the quiz.
+    At the end of the quiz, the user should be shown their score, and the correct answers to any questions they answered incorrectly, and the time taken to complete the quiz in minutes. 
+
+Here's some example questions, or you may use your own:
+
+    Q: What is the state capital of California? A: Sacramento
+    Q: What is the tallest mountain in the world? A: Everest
+    Q: Which country has the furthest south extent?: A: Chile
+
+Design your app in class. Draw sketches of your Form and describe the way your app will work. How will your app check for correct answers? How will it store incorrect answers, and the user's score? How will it figure out how long it takes the user to complete the quiz? What else do you need to think about?
+
+Check in with me in class before you start coding.  You must get your design approved in class before you start coding. 
+
+Finish the code and testing of your app individually.
+
+Make sure you add comments describing your method and reasoning for how you decide if an answer is correct. How will you balance accepting correct answers, with the chance of permitting wrong answers?
+ */
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Geography_Quiz_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCheckAnswer_Click(object sender, EventArgs e)
+        {
+            List<string> quizAnswers = new List<string>();
+            
+            string answer = txtAnswer1.Text;
+            if (answer.Contains("Sacramento"))
+            {
+                MessageBox.Show("Correct!", "Result");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect, the answer is Sarcramento", "Result");
+            }
+
+            string answer2 = txtAnswer2.Text;
+            if (answer2.Contains("Everest"))
+            {
+                MessageBox.Show("Correct!", "Result");
+            }
+            else
+            {
+                MessageBox.Show("Incorrect, the answer is Everest", "Result");
+            }
+
+            string answer3 = txtAnswer3.Text;
+            if (answer3.Contains("Chile")) 
+            {
+                MessageBox.Show("Correct!", "Result");
+            }
+            else 
+            {
+                MessageBox.Show("Incorrect, the answer is Chile", "Result");
+            }
+        }
+    }
+ }
