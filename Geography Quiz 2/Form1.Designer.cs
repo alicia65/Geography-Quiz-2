d5aec58089ebbd5f9663f2e1466cf39f53021843
alicia65 <alicia65>
2020-04-08 16:41:42 -0500

    Add more text boxes and revise codes

--- Geography Quiz 2/Form1.Designer.cs
+++ Geography Quiz 2/Form1.Designer.cs
@@ -31,40 +31,49 @@
             this.btnCheckAnswer = new System.Windows.Forms.Button();
             this.txtQuestions = new System.Windows.Forms.TextBox();
             this.lblScore = new System.Windows.Forms.Label();
+            this.txtUserAnswer = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // btnCheckAnswer
             // 
-            this.btnCheckAnswer.Location = new System.Drawing.Point(185, 102);
+            this.btnCheckAnswer.Location = new System.Drawing.Point(150, 158);
             this.btnCheckAnswer.Name = "btnCheckAnswer";
             this.btnCheckAnswer.Size = new System.Drawing.Size(132, 23);
-            this.btnCheckAnswer.TabIndex = 1;
+            this.btnCheckAnswer.TabIndex = 2;
             this.btnCheckAnswer.Text = "Check answer";
             this.btnCheckAnswer.UseVisualStyleBackColor = true;
             this.btnCheckAnswer.Click += new System.EventHandler(this.btnCheckAnswer_Click);
             // 
             // txtQuestions
             // 
-            this.txtQuestions.Location = new System.Drawing.Point(76, 38);
+            this.txtQuestions.Location = new System.Drawing.Point(25, 36);
             this.txtQuestions.Name = "txtQuestions";
             this.txtQuestions.ReadOnly = true;
-            this.txtQuestions.Size = new System.Drawing.Size(405, 22);
+            this.txtQuestions.Size = new System.Drawing.Size(363, 22);
             this.txtQuestions.TabIndex = 0;
             // 
             // lblScore
             // 
-            this.lblScore.Location = new System.Drawing.Point(205, 173);
+            this.lblScore.Location = new System.Drawing.Point(156, 220);
             this.lblScore.Name = "lblScore";
             this.lblScore.Size = new System.Drawing.Size(100, 23);
-            this.lblScore.TabIndex = 5;
+            this.lblScore.TabIndex = 3;
             this.lblScore.Text = "Score: ?";
             this.lblScore.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtUserAnswer
+            // 
+            this.txtUserAnswer.Location = new System.Drawing.Point(105, 98);
+            this.txtUserAnswer.Name = "txtUserAnswer";
+            this.txtUserAnswer.Size = new System.Drawing.Size(220, 22);
+            this.txtUserAnswer.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(684, 291);
+            this.Controls.Add(this.txtUserAnswer);
             this.Controls.Add(this.lblScore);
             this.Controls.Add(this.txtQuestions);
             this.Controls.Add(this.btnCheckAnswer);
@@ -79,6 +88,7 @@
         private System.Windows.Forms.Button btnCheckAnswer;
         private System.Windows.Forms.TextBox txtQuestions;
         private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.TextBox txtUserAnswer;
     }
 }
 
--- Geography Quiz 2/Form1.cs
+++ Geography Quiz 2/Form1.cs
@@ -5,7 +5,9 @@ Start a new app.
     This app will ask at least three geography questions, one question at a time.
     The app will keep track of how many questions the user answers correctly. 
     Your app will need to track how long the user took to complete the quiz.
-    At the end of the quiz, the user should be shown their score, and the correct answers to any questions they answered incorrectly, and the time taken to complete the quiz in minutes. 
+    At the end of the quiz, the user should be shown their score, 
+    and the correct answers to any questions they answered incorrectly,
+    and the time taken to complete the quiz in minutes. 
 
 Here's some example questions, or you may use your own:
 
@@ -13,13 +15,18 @@ Here's some example questions, or you may use your own:
     Q: What is the tallest mountain in the world? A: Everest
     Q: Which country has the furthest south extent?: A: Chile
 
-Design your app in class. Draw sketches of your Form and describe the way your app will work. How will your app check for correct answers? How will it store incorrect answers, and the user's score? How will it figure out how long it takes the user to complete the quiz? What else do you need to think about?
+Design your app in class. Draw sketches of your Form and describe the way your app will work.
+How will your app check for correct answers? How will it store incorrect answers, 
+and the user's score? How will it figure out how long it takes the user to complete the quiz? 
+What else do you need to think about?
 
-Check in with me in class before you start coding.  You must get your design approved in class before you start coding. 
+Check in with me in class before you start coding. 
+You must get your design approved in class before you start coding. 
 
 Finish the code and testing of your app individually.
 
-Make sure you add comments describing your method and reasoning for how you decide if an answer is correct. How will you balance accepting correct answers, with the chance of permitting wrong answers?
+Make sure you add comments describing your method and reasoning for how you decide if an answer is correct. 
+How will you balance accepting correct answers, with the chance of permitting wrong answers?
  */
 
 using System;
@@ -38,14 +45,14 @@ namespace Geography_Quiz_2
     {
         //Generate global variables for the quiz to use in the app
 
-        //Create a SortList with data types as strings for key-value pair.  
+        //Create a SortList with data type as strings for key-value pair.  
         //Keys are questions.  Values are answers.
         //Queries is the name of the SortList and contains three elements
         private SortedList<string, string> queryList = new SortedList<string, string>()
         { {"What is the state capital of California?","Sacramento" },
             {"What is the tallest mountain in the world?", "Everest"},
             {"Which country has the furthest south extent?", "Chile"} };
-       
+
         private int queryNumber = -1;
         private int Score = 0; 
         
@@ -62,16 +69,16 @@ namespace Geography_Quiz_2
 
         private void btnCheckAnswer_Click(object sender, EventArgs e)
         {
-            ShowMyNextQuestion();
-            CheckKey();
+            ShowMyNextQuestion();//call method to show next question
+            CheckKey();//call key to check for correct answer
 
         }
 
         private void ShowMyNextQuestion() 
         {
-            queryNumber++;
+            queryNumber++;// declaring variable to move to next question
 
-            if(queryNumber< queryList.Count) 
+            if(queryNumber< queryList.Count)//checking each question in the SortedList 
             {
                 KeyValuePair<string, string> query = queryList.ElementAt(queryNumber);
                 string queryTxt = query.Key;
@@ -86,20 +93,36 @@ namespace Geography_Quiz_2
             
         }
 
-        private void CheckKey() 
+        //Checking 
+        private void CheckKey()
         {
-            if(queryNumber < queryList.Count) 
+            try
             {
-                KeyValuePair<string, string> inquiry = queryList.ElementAt(queryNumber);
-                string correctAnswer = inquiry.Value;
-
-                if(correctAnswer == txtQuestions.ContainsKey(string)) 
+                if (queryNumber < queryList.Count)//
                 {
-                    Score++;
-                }
-                lblScore.Text = $"Score: {Score}";
+                    KeyValuePair<string, string> inquiry = queryList.ElementAt(queryNumber);
+                    string correctAnswer = inquiry.Value;
 
+                    if (txtQuestions.Text.Contains(correctAnswer))
+                    {
+                        Score++;
+                    }
+                    lblScore.Text = $"Score: {Score}";
+
+                    if (txtUserAnswer.Text.Contains(correctAnswer))// checking user input for right answer
+                    {
+                        Score++;// tally user's corrected response
+                    }
+                    lblScore.Text = $"Score: {Score}";//label score displays scores
+                }
             }
+            catch (FormatException fe)
+            {
+                MessageBox.Show("Please enter valid letters only for the answer field.", "Entry Error");
+                txtUserAnswer.Focus();
+                throw fe;
+            }
+
         }
         
     }
